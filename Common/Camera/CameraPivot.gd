31a78b14cb9820436b82eeea8a4d18a21df6effb
gappy <g4pci02137@gmail.com>
2025-03-25 15:53:44 +0100

    wind shader

--- Common/Camera/CameraPivot.gd
+++ Common/Camera/CameraPivot.gd
@@ -6,24 +6,30 @@ var mouse_sensitivity: float = 0.5;
 @onready var camera_yaw: Node3D = $CameraYaw
 @onready var camera_pitch: Node3D = $CameraYaw/CameraPitch
 @onready var spring_arm: SpringArm3D = $CameraYaw/CameraPitch/SpringArm
-@onready var camera: Camera3D = $CameraYaw/CameraPitch/SpringArm/Camera
+#@onready var camera: Camera3D = $CameraYaw/CameraPitch/SpringArm/Camera
+@onready var camera: Camera3D = $CameraYaw/CameraPitch/Camera
 
 var yaw: float = 0.0;
 var pitch: float = 0.0;
 var pitch_max : float = 45;
 var pitch_min : float = -45;
-var position_offset : Vector3 = Vector3(0, 1.3, 0);
-var position_offset_target : Vector3 = Vector3(0, 1.3, 0);
+var position_offset : Vector3 = Vector3(0, 6, 10);
+var position_offset_target : Vector3 = Vector3(0, 6, 10);
+
+var camera_pos: Vector3 = Vector3(0.0, 8.0, 18.0);
+var camera_zoom: Vector3 = Vector3(0.0, 0.0, 0.0);
 
 var zoom: float = 7.0;
 var zoom_min: float = 4.0;
 var zoom_max: float = 12.0;
 
 func _ready() -> void:
+	pass
 	spring_arm.add_excluded_object(player.get_rid())
-	top_level = true;
+	#top_level = true;
 
 func _unhandled_input(event: InputEvent) -> void:
+	pass
 	
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
@@ -46,23 +52,25 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("camera_zoom_in"):
-		zoom -= 1.0;
+		camera_zoom -= Vector3(0.0, 0.375, 1.0);
 	if Input.is_action_just_pressed("camera_zoom_out"):
-		zoom += 1.0;
-		
-	zoom = clamp(zoom, zoom_min, zoom_max);
-	print(zoom)
-		
-	spring_arm.spring_length = lerp(spring_arm.spring_length, zoom, 4 * delta);
-	camera.position.z = spring_arm.spring_length;
+		camera_zoom += Vector3(0.0, 0.375, 1.0);
 	
-	position_offset = lerp(position_offset, position_offset_target, 4 * delta)
-	global_position = lerp(global_position, player.global_position + position_offset, 18 * delta)
+	camera_zoom = clamp(camera_zoom, Vector3(0.0, -5.25, -14.0), Vector3(0.0, 2.25, 6.0));
+	#zoom = clamp(zoom, zoom_min, zoom_max);
+		
+	#spring_arm.spring_length = lerp(spring_arm.spring_length, zoom, 4 * delta);
+	#camera.position.z = spring_arm.spring_length;
 	
-	pitch = clamp(pitch, pitch_min, pitch_max)
+	#camera.fov = lerp(camera.fov, 75.0 + zoom, 4 * delta)
+	
+	#camera_pos = lerp(camera_pos, position_offset_target, 4 * delta);
+	global_position = lerp(global_position, player.global_position + camera_zoom, 18 * delta);
+	
+	#pitch = clamp(pitch, pitch_min, pitch_max)
 	
 	#camera_yaw.rotation_degrees.y = yaw
 	#camera_pitch.rotation_degrees.x = pitch	
 	#
-	camera_yaw.rotation_degrees.y = lerp(camera_yaw.rotation_degrees.y, yaw, mouse_sensitivity * delta * 30)
-	camera_pitch.rotation_degrees.x = lerp(camera_pitch.rotation_degrees.x, pitch, mouse_sensitivity * delta * 30)
+	#camera_yaw.rotation_degrees.y = lerp(camera_yaw.rotation_degrees.y, yaw, mouse_sensitivity * delta * 30)
+	#camera_pitch.rotation_degrees.x = lerp(camera_pitch.rotation_degrees.x, pitch, mouse_sensitivity * delta * 30)
---
+++ Common/WeatherManager/WeatherManager.gd
@@ -0,0 +1,28 @@
+extends Node3D
+class_name CLASSWeatherManager
+
+@export var wind_texture: NoiseTexture2D;
+var noise_offset: Vector3;
+var noise: FastNoiseLite;
+
+var wind_pos: float = 0.0;
+
+func _ready() -> void:
+	noise = wind_texture.noise;
+	noise_offset = noise.offset;
+
+func _process(delta: float) -> void:
+	
+	wind_pos += 0.1;
+	
+	noise_offset = Vector3(wind_pos, wind_pos, 0.0);
+	
+	wind_texture.noise.offset = noise_offset;
+
+	#for node in get_tree().get_nodes_in_group("vegetation"):
+		#var onscreen: VisibleOnScreenNotifier3D = node.find_child("OnScreen")
+		#if onscreen:
+			#if !onscreen.is_on_screen():
+				#node.visible = false;
+			#else:
+				#node.visible = true;
